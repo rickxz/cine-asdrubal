7793a7bd06ca0daead93d803d60120ad69b4ddc3
rickxz <herick.victor.rodrigues@hotmail.com>
2024-10-26 19:01:01 -0300

    feat(view-movie): improve loading conditional rendering

--- src/pages/view-movie.tsx
+++ src/pages/view-movie.tsx
@@ -5,18 +5,27 @@ import { Button } from '@/components/ui/button'
 import { useNavigate } from 'react-router-dom'
 
 export function ViewMovie() {
-  const { movie, isFetching } = useDoesMovieExists()
+  const { movie } = useDoesMovieExists()
   const navigate = useNavigate()
 
   return (
     <div className="container mx-auto px-4 py-8 flex flex-col items-center">
-      {isFetching && <Skeleton className="w-[300px] h-[40px]" />}
-      <h1 className="text-4xl font-bold mb-8">{movie?.name}</h1>
-      <div className="w-full max-w-[300px] space-y-6">
-        {isFetching && <Skeleton className="w-full h-[450px]" />}
-        { movie && <MovieCard movie={movie} alwaysDisplayInfo /> }
-        { movie && <Button variant="outline" onClick={() => navigate('/')}>Voltar</Button>}
-      </div>
+      {!movie ? (
+        <>
+          <Skeleton className="w-[300px] h-[40px] mb-8" />
+          <div className="w-full max-w-[300px] space-y-6">
+            <Skeleton className="w-full h-[450px]" />
+          </div>
+        </>
+      ) : (
+        <>
+          <h1 className="text-4xl font-bold mb-8">{movie?.name}</h1>
+          <div className="w-full max-w-[300px] space-y-6">
+            <MovieCard movie={movie} alwaysDisplayInfo />
+            <Button variant="outline" onClick={() => navigate('/')}>Voltar</Button>
+          </div>
+        </>
+      )}
     </div>
   )
 }
